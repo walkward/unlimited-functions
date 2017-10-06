8b24cb9853a7e51ad8bc773b0d446046391d6be9
Walker Ward <walker.ward1@gmail.com>
2017-10-05 23:00:44 -0600

    updates

--- addTags/index.js
+++ addTags/index.js
@@ -17,7 +17,7 @@ exports.addTags = function addTags(req, res) {
   var storeName = 'darxe.myshopify.com'; // SETUP: Add store domain
 
   var authorization = 'Basic ' + new Buffer(apiKey + ':' + apiSecret).toString('base64');
-  var hmac = req.headers['X-Shopify-Hmac-Sha256'];
+  var hmac = req.headers;
   var productId = req.body.id;
   var newTags = 'tags added, tag\'s now';
 
