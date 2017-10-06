ba7cc0de2ee16812639c5d47ea33bce903750f68
Walker Ward <walker.ward1@gmail.com>
2017-10-05 19:54:52 -0600

    updates

--- hello-world/index.js
+++ hello-world/index.js
@@ -12,18 +12,18 @@
 exports.helloWorld = function helloWorld(req, res) {
   // The Shopify app's shared secret, viewable from the Partner dashboard
   var sharedSecret = "fb2110342a563e14495e5178289609166dd7c0a6eafbda88a16b0761213252fd";
-  var retrievedSignature = JSON.stringify(req.headers['x-shopify-hmac-sha256']);
+  var hmac = JSON.stringify(req.headers['x-shopify-hmac-sha256']);
 
   // Verify the Shopify webhook's integrity
-  // function verifyShopifyHook(req) {
-  //     var digest = crypto.createHmac('SHA256', sharedSecret)
-  //             .update(new Buffer(req.body, 'utf8'))
-  //             .digest('base64');
-  //
-  //     return digest === req.headers['x-shopify-hmac-sha256'];
-  // }
+  function verifyShopifyHook(req) {
+      var digest = crypto.createHmac('SHA256', sharedSecret)
+              .update(new Buffer(req.body, 'utf8'))
+              .digest('base64');
 
-  if (req.body){
+      return digest === hmac;
+  }
+
+  if (verifyShopifyHook(req)){
     // Everything is okay.
     console.log(req.body);
     res.status(200).send('Success: ' + req.body);
